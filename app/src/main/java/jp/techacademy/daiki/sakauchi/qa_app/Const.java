3727d1f44ed96ed2ebcaeca8248c268c7ef9dd85
SakauchiDaiki <daiki.sakauchi@g.softbank.co.jp>
2018-02-08 10:35:03 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/daiki/sakauchi/qa_app/Const.java
@@ -0,0 +1,15 @@
+package jp.techacademy.daiki.sakauchi.qa_app;
+
+// 複数のクラスから使う定数を定義
+
+/**
+ * Created by sakauchidaiki on 2018/02/02.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
---
+++ app/src/main/java/jp/techacademy/daiki/sakauchi/qa_app/Cost.java
@@ -0,0 +1,8 @@
+package jp.techacademy.daiki.sakauchi.qa_app;
+
+/**
+ * Created by sakauchidaiki on 2018/02/02.
+ */
+
+public class Cost {
+}
